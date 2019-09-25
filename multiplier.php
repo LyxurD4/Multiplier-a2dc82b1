f5c9b0ffbe616aed60c78cc2e7d64c04edfed23e
MAURITS DOES <2111633@talnet.nl>
2019-09-25 14:26:45 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,9 @@
+<?php
+echo "Computers zijn tt goed in rekenen a neef\n";
+echo "Geef me je eerste cijfer bro. Ze gaan wel keer elkaar. De rest kan ik nog niet\n";
+$een = readline();
+echo "ait ait. nu je tweede\n";
+$twee = readline();
+echo "Even nadenken.... één keer twee..... 5 onthouden ... en dan denk ik... : ". $een * $twee;
+echo "\nHad ik het goed?"
+?> 
